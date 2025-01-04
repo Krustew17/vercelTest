c7d074be0a7c61c8b33ab6f5b87c2b239e3fba0e
krustew17 <krasimircho555@gmail.com>
2025-01-04 16:13:06 +0200

    test ci/cd for fe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import paths from "./config/paths.js";
 function App() {
     return (
         <Routes>
-            <Route path={paths.home} element={<h1>Home</h1>} />
+            <Route path={paths.home} element={<h1>Home yueye</h1>} />
         </Routes>
     );
 }
