c29446f0aef75204b4f56819c8c42ad3c569c257
krustew17 <krasimircho555@gmail.com>
2025-01-04 17:31:57 +0200

    testing ci/cd once again

--- server/index.js
+++ server/index.js
@@ -9,4 +9,10 @@ const app = express();
 
 appConfig(app);
 
+if (process.env.NODE_ENV === "development") {
+    app.listen(process.env.APP_PORT, () => {
+        console.log(`Server running on port ${process.env.APP_PORT}`);
+    });
+}
+
 export default app;
